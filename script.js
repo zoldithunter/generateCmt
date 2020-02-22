239d03b5a13ec6b603db50b2b7c58081f9c99fa2
zoldkyllua <zold.ithunter@gmail.com>
2020-02-22 16:54:29 +0700

    update

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 var _listRule = [];
 $( document ).ready(function() {
-
-    // readTextFile("rule.txt");
-    loadXMLDoc();
-
+    readRuleFile("rule.txt");
     $('#run').click(function(){
         let fileData = document.getElementById('loadData').files[0];
         if (fileData) {
@@ -77,30 +74,26 @@ function loadRule(file) {
     reader.readAsText(file.files[0]);
 }
 
-function readTextFile(filePath)
-{
-    let file = new File(["string"], filePath);
-    let reader = new FileReader();
-    reader.onload = function(progressEvent) {
-        let lines = this.result.split('\n');
-        console.log(lines);
+function readRuleFile(filePath) {
+    var xmlhttp;
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        // code for older browsers
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
-    reader.readAsText(file);
-}
-
-function loadXMLDoc() {
-var xmlhttp;
-if (window.XMLHttpRequest) {
-xmlhttp = new XMLHttpRequest();
-} else {
-// code for older browsers
-xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-}
-xmlhttp.onreadystatechange = function() {
-if (this.readyState == 4 && this.status == 200) {
-console.log(this.responseText);
-}
-};
-xmlhttp.open("GET", "rule.txt", true);
-xmlhttp.send();
+        xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            $('#body').html("");
+            let lines = this.responseText.split('\n');
+            [...lines].forEach((ele, i) => {
+                let line = lines[i].split('|');
+                let rule = line.splice(0,1);
+                let data = line.join("|");
+                $('#body').append("<tr><td>Rule" + (i + 1) + ":</td><td><input id = 'rule" + i + "' name='rule' value='" + rule +"'></td><td><input id = 'text" + i +"' name='value' value='"+data+"' ></td></tr>");
+            })
+        }
+    };
+    xmlhttp.open("GET", filePath, true);
+    xmlhttp.send();
 }
